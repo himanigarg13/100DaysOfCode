a8bf2451b886017fd4e717374cdd50af03e05ccf
himanigarg13 <113790240+himanigarg13@users.noreply.github.com>
2022-10-24 16:31:39 +0530

    Create JavaStringTokens.java

---
+++ Day10_StringTokens/JavaStringTokens.java
@@ -0,0 +1,31 @@
+//Given a string,s, matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens. We define a token to be one or more consecutive English alphabetic letters. Then, print the number of tokens, followed by each token on a new line.
+
+//Note: You may find the String.split method helpful in completing this challenge.
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+   
+    String[] arrs = s.trim().split("[ !,?._'@]+");
+    if(s.trim().isEmpty())
+    {
+        System.out.println("0");
+    }
+    else
+    {
+    System.out.println(arrs.length);
+    for(String x : arrs)
+    {
+        System.out.println(x);
+    }
+    }
+        scan.close();
+    }
+}
+
