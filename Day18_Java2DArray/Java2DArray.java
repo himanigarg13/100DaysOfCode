17424a781710a21844f2d999af03ccdff1db9a83
himanigarg13 <113790240+himanigarg13@users.noreply.github.com>
2022-11-03 13:35:36 +0530

    Create Java2DArray.java

---
+++ Day18_Java2DArray/Java2DArray.java
@@ -0,0 +1,44 @@
+//The sum of an hourglass is the sum of all the numbers within it. The sum for the hourglasses above are 7, 4, and 2, respectively.
+//In this problem we have to print the largest sum among all the hourglasses in the array.
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+public class solution {
+    
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+    int csum =Integer.MIN_VALUE;
+        for(int i=0;i<6;i++)
+        {
+            for(int j=0;j<6;j++)
+            { 
+                if(i>1 && j>1)
+                {
+                    int sum=arr[i][j]+arr[i-1][j-1]+arr[i-2][j]+arr[i-2][j-1]
+                    +arr[i-2][j-2]+arr[i][j-1]+arr[i][j-2];
+                    
+                    if(sum>csum)
+                    {
+                        csum=sum;
+                    }
+                }
+            }
+        }
+        System.out.print(csum);
+        scanner.close();
+    }
+}
