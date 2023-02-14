770588f91a88dcce65ce1bbcfb36673584df7c6e
himanigarg13 <113790240+himanigarg13@users.noreply.github.com>
2023-02-14 11:09:17 +0530

    Create 0_on_left_side.java

---
+++ Day77_0_on_left_side/0_on_left_side.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class segregate {
+    public static void main(String args[]) {
+        //Scanner sc=new Scanner(System.in);
+       int n=5;
+        int arr[]={0,1,0,1,0};
+        int left=0,right=n-1;
+       
+         while(true){
+            if(left>=right){
+                break;
+            }
+        {
+            if(arr[left]==0){
+                left++;
+            }
+            else if(arr[right]==1){
+                right--;
+            }
+            else{
+                arr[left]=0;
+                arr[right]=1;
+                left++;
+                right--;
+            }
+         }
+         
+    }
+    for(int i=0;i<n;i++){
+        System.out.println(arr[i]);
+     }
+}
+}
